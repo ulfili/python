7a1bc66919e188d4704daa653a54a55cab6438c2
Uljana <ulfili@ttu.ee>
2022-08-31 20:36:41 +0300

    kirjutasin programmi, mis küsib nime ja numbreid ning liidab numbrid kokku

--- EX/ex00_intro/hello.py
+++ EX/ex00_intro/hello.py
@@ -4,7 +4,9 @@ name = input("Hello, what is your name? ")                  # ask for a name
 print("Hi,", name)                                          # say hello and name
 a = int(input("Enter a random number: "))                   # ask for a first number
 b = int(input("Great! Now enter a second random number: ")) # ask for a seccond number
-print(a,"+",b, "is", a+b)                                   # tipe an answer
+print(a,"+",b, "is", a + b)                                 # tipe an answer
+
+
 
 
 
