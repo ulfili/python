1cba4682613f03c44d4d21bf76934fac31a3c439
Uljana <ulfili@ttu.ee>
2022-08-31 23:08:39 +0300

    Done exercise

--- EX/ex01_hello/greetings.py
+++ EX/ex01_hello/greetings.py
@@ -1,6 +1,7 @@
 """EX01 Greetings."""
 
-greeting = input("please enter a greating: ")
-recipient = input("please enter a recipient: ")
-repeat = int(input("How many times to repeat? " ))
-print((greeting + recipient) * repeat)
+greeting = input("please enter a greeting: ")        # enter greeting
+recipient = input("please enter a recipient: ")      # enter recipient
+repeat = int(input("How many times to repeat? "))    # enter number of repeats
+welcome = (greeting + " " + recipient + "! ")        # concatenation greeting and recipient with space
+print(welcome * repeat)                              # type welcome message repeat times
