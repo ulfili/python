00aa58592c861bc9097d08f1113e2d3c8a6da9b5
Uljana <ulfili@ttu.ee>
2022-08-31 22:55:13 +0300

    First commit of exersice

---
+++ EX/ex01_hello/greetings.py
@@ -0,0 +1,6 @@
+"""EX01 Greetings."""
+
+greeting = input("please enter a greating: ")
+recipient = input("please enter a recipient: ")
+repeat = int(input("How many times to repeat? " ))
+print((greeting + recipient) * repeat)
