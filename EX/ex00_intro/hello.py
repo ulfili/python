a71327e5554afa163c20157c742749903af5d94a
Uljana <ulfili@ttu.ee>
2022-08-31 20:42:14 +0300

    kirjutasin programmi, mis küsib nime ja numbreid ning liidab numbrid kokku

--- EX/ex00_intro/hello.py
+++ EX/ex00_intro/hello.py
@@ -1,10 +1,11 @@
 """EX01 Hello."""
 
-name = input("Hello, what is your name? ")                  # ask for a name
-print("Hi,", name)                                          # say hello and name
-a = int(input("Enter a random number: "))                   # ask for a first number
-b = int(input("Great! Now enter a second random number: ")) # ask for a seccond number
-print(a,"+",b, "is", a + b)                                 # tipe an answer
+name = input("Hello, what is your name? ")                      # ask for a name
+print("Hi,", name)                                              # say hello and name
+a = int(input("Enter a random number: "))                       # ask for a first number
+b = int(input("Great! Now enter a second random number: "))     # ask for a second number
+print(a, "+", b, "is", a + b)                                   # tipe an answer
+
 
 
 
