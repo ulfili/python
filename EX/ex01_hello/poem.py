931a1ab3832d61a9a6f9832c92d424f2a2b37fee
Uljana <ulfili@ttu.ee>
2022-08-31 22:28:34 +0300

    first revision of exercise

---
+++ EX/ex01_hello/poem.py
@@ -0,0 +1,9 @@
+"""EX01 Poem."""
+
+color = input("please enter a color: ")            # ask for a color
+objects = input("please enter plural noun: ")      # ask for a plural noun
+activity = input("please enter an activity: ")     # ask for activity
+print("Roses are", str(color)+",")                 # use str to remove leading spaces in color
+print(objects, "are blue,")                        # type blue object
+print("I love to", activity)                       # type an activity
+print("And so will you!")                          # type end of the poem
