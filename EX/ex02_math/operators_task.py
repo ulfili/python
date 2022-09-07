ac392c91d97f6993714da06150c1feff3e8d39e7
Uljana <ulfili@ttu.ee>
2022-09-07 22:01:12 +0300

    first try

--- EX/ex02_math/operators_task.py
+++ EX/ex02_math/operators_task.py
@@ -1,30 +1,95 @@
 """Math operators."""
+import math                                   # open math modul
 
-def add(x: int, y: int) -> int:
+
+def add(x: int, y: int):
     return x + y
-def sub(x: int, y: int) -> int:
+
+
+print(add(1, 2))
+
+
+def sub(x: int, y: int):
     return x - y
-def multiply(x: int, y: int) -> int:
+
+
+print(sub(1, 2))
+
+
+def multiply(x: int, y: int):
     return x * y
-def div(x: int, y: int) -> int:
+
+
+print(multiply(1, 2))
+
+
+def div(x: int, y: int):
     return x / y
-def modulus(x: int, y: int) -> int:
+
+
+print(div(1, 2))
+
+
+def modulus(x: int, y: int):
     return x % y
-def floor_div(x: int, y: int) -> int:
+
+
+print(modulus(1, 2))
+
+
+def floor_div(x: int, y: int):
     return x // y
-def exponent(x: int, y: int) -> int:
+
+
+print(floor_div(1, 2))
+
+
+def exponent(x: int, y: int):
     return math.pow(x, y)
-def first_greater_or_equal(x: int, y: int) -> int:
 
 
+print(math.pow(2, 3))
 
-if __name__ == '__main__':
-    print(add(1, 2))
-    print(sub(1, 2))
-    print(multiply(1, 2))
-    print(div(1, 2))
-    print(modulus(1, 2))
-    print(floor_div(1, 2))
-    import math
-    print(math.pow(2, 3))
+
+def first_greater_or_equal(x: int, y: int):
+    return x >= y
+
+
+print(first_greater_or_equal(4, 3))
+
+
+def second_less_or_equal(x: int, y: int):
+    return y <= x
+
+
+print(second_less_or_equal(4, 3))
+
+
+def x_is_y(x: int, y: int):
+    return x == y
+
+
+print(x_is_y(2, 2))
+
+
+def x_is_not_y(x: int, y: int):
+    return x != y
+
+
+print(x_is_not_y(2,4))
+
+
+def if_else(a: int, b: int, c: int, d: int):
+    mult = a * b
+    div = c / d
+    if mult == div:
+        return 0
+    if mult > div:
+        return mult
+    return div
+
+
+print(if_else(2, 2, 8, 2))
+print(if_else(8, 4, 2, 1))
+print(if_else(1,1,9,3))
 
