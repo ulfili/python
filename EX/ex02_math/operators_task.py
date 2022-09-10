c6d4bcd6aa54757377f64233248a09ef25627463
Uljana <ulfili@ttu.ee>
2022-09-10 11:53:26 +0300

    docstring added

--- EX/ex02_math/operators_task.py
+++ EX/ex02_math/operators_task.py
@@ -1,53 +1,55 @@
 """Math operators."""
-import math                                     # open math modul
+import math                                                           # open math modul
 
 
-def add(x: int, y: int):                        # + action
+def add(x: int, y: int):                                              # + action
+    """Add x to y."""
     return x + y
 
 
-def sub(x: int, y: int):                        # - action
+def sub(x: int, y: int):                                              # - action
+    """Subtract y from x."""
     return x - y
 
 
-def multiply(x: int, y: int):                   # * action
+def multiply(x: int, y: int):                                         # * action
     return x * y
 
 
-def div(x: int, y: int):                        # / action
+def div(x: int, y: int):                                              # / action
     return x / y
 
 
-def modulus(x: int, y: int):                    # % action
+def modulus(x: int, y: int):                                          # % action
     return x % y
 
 
-def floor_div(x: int, y: int):                  # // action
+def floor_div(x: int, y: int):                                        # // action
     return x // y
 
 
-def exponent(x: int, y: int):                   # exponent
+def exponent(x: int, y: int):                                         # exponent
     return math.pow(x, y)
 
 
-def first_greater_or_equal(x: int, y: int):     # x is bigger or same as y
+def first_greater_or_equal(x: int, y: int):                           # x is bigger or same as y
     return x >= y
 
 
-def second_less_or_equal(x: int, y: int):       # x is bigger or same as y
+def second_less_or_equal(x: int, y: int):                             # x is bigger or same as y
     return y <= x
 
 
-def x_is_y(x: int, y: int):                     # x and y are the same
+def x_is_y(x: int, y: int):                                           # x and y are the same
     return x == y
 
 
-def x_is_not_y(x: int, y: int):                 # x is not y
+def x_is_not_y(x: int, y: int):                                       # x is not y
     return x != y
 
 
-def if_else(a: int, b: int, c: int, d: int):    # if multiplied a and b is same as divided c and d then return 0
-    mult = a * b                                # or return bigger result
+def if_else(a: int, b: int, c: int, d: int):                          # if multiplied a and b is same as divided c and d then return 0
+    mult = a * b                                                      # or return bigger result
     div = c / d
     if mult == div:
         return 0
@@ -56,15 +58,15 @@ def if_else(a: int, b: int, c: int, d: int):    # if multiplied a and b is same 
     return div
 
 
-def surface(a: int, b: int):
+def surface(a: int, b: int):                                          # find rectangle area
     return a * b
 
 
-def volume(aa: int, bb: int, cc: int):
+def volume(aa: int, bb: int, cc: int):                                # find cuboid volume
     return aa * bb * cc
 
 
-def clock(day: int, hour: int, minut: int, sec: int) -> float:
+def clock(day: int, hour: int, minut: int, sec: int) -> float:        # return the total amount of minutes
     day = day * 1440
     hour = hour * 60
     minut = minut
@@ -72,7 +74,7 @@ def clock(day: int, hour: int, minut: int, sec: int) -> float:
     return day + hour + minut + sec
 
 
-def calculate(act: int, num_1: int, num_2: int):
+def calculate(act: int, num_1: int, num_2: int):                      # calculate function
     if act == 0:
         return num_1 + num_2
     if act == 1:
