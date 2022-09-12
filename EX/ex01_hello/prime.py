50bbb0f520ad84e158591ed85db27538a9ed7edb
Uljana <ulfili@ttu.ee>
2022-09-12 14:15:47 +0300

    first try of prime ex

---
+++ EX/ex01_hello/prime.py
@@ -0,0 +1,11 @@
+"""Prime number identifier."""
+
+def is_prime_number(k: int) -> bool:
+    for i in range(2, k + 1):
+        if k % i == 0:
+            return False
+        if k % i >= 0:
+            return True
+
+
+print(is_prime_number(5))
