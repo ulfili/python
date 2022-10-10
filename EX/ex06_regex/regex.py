a8e9694db45df0e6215ad738bb70ca0d4d0201ff
Uljana <ulfili@ttu.ee>
2022-10-10 17:35:11 +0300

    find words only function

--- EX/ex06_regex/regex.py
+++ EX/ex06_regex/regex.py
@@ -3,19 +3,7 @@ import re
 
 
 def find_words(text: str) -> list:
-    """
-    Given string text, return all the words in that string.
-
-    A word here is considered to be any combination letters that starts with
-    a capital letter and contains of at least one more lowercase letter.
-    Note that Estonian õ, ä, ö and ü should also be accepted here.
-
-    Words must be found using regex.
-
-    :param text: given string
-     find words from
-    :return: list of words found in given string
-    """
+    """Given string text, return all the words in that string."""
     new_list = []
     pattern = r"[A-ZÕÄÖÜ][a-zõäöü]*"
     for match in re.finditer(pattern, text):
@@ -28,18 +16,7 @@ print(find_words("Tomat,Apelsin,mandarin,Klaviatuur,arvuti,Äike,Päike"))
 
 
 def find_words_with_vowels(text: str) -> list:
-    """
-    Given string text, return all the words in that string that start with a vowel.
-
-    A word here is considered to be any combination letters that starts with
-    a capital letter and contains of at least one more lowercase letter.
-    Note that Estonian õ, ä, ö and ü should also be accepted here.
-
-    Words must be found using regex.
-
-    :param text: given string to find words from
-    :return: list of words that start with a vowel found in given string
-    """
+    """Given string text, return all the words in that string that start with a vowel."""
     new_list = []
     pattern = r"[AEIOUÕÄÖÜ][a-zõäöü]*"
     for match in re.finditer(pattern, text):
@@ -52,18 +29,7 @@ print(find_words_with_vowels("Tomat,Apelsin,mandarin,Klaviatuur,arvuti,Äike,Pä
 
 
 def find_sentences(text: str) -> list:
-    """
-    Given string text, return all sentences in that string.
-
-    A sentence always starts with a capital letter and ends with punctuation (.!?).
-    Note that a sentence may also contain all the typical symbols (like commas, colons, numbers, etc.).
-    A sentence may also end in multiple punctuation (example: "Wait...").
-
-    Sentences must be found using regex.
-
-    :param text: given string to find sentences from
-    :return: list of sentences found in given string
-    """
+    """Given string text, return all sentences in that string."""
     new_list = []
     pattern = r"([A-ZÕÄÖÜ][^\.!?]*[\.!?])"
     for match in re.finditer(pattern, text):
@@ -76,19 +42,7 @@ print(find_sentences("See on:esimene lause! See lause on 2. Aga see läuse on - 
 
 
 def find_words_from_sentence(sentence: str) -> list:
-    """
-    Given a sentence, return all words in that sentence.
-
-    Here, a word is considered to be a normal word in a sentence,
-    that is separated from other words by a whitespace (or commas, etc.).
-    Note that numbers are also considered as words here, but commas, etc. are not
-    a part of a word.
-
-    Words must be found using regex.
-
-    :param sentence: given sentence to find words from
-    :return: list of words found in given sentence
-    """
+    """Given a sentence, return all words in that sentence."""
     new_list = []
     pattern = r"\w+"
     for match in re.finditer(pattern, sentence):
@@ -98,3 +52,32 @@ def find_words_from_sentence(sentence: str) -> list:
 
 
 print(find_words_from_sentence("Minu nimi on - uljana."))
+
+
+def find_words_from_sentences_only(text: str) -> list:
+    """Given string text, return all words in that string that are a part of a sentence in that string."""
+    new_list = []
+    sentences_only = find_sentences(text)
+    print(sentences_only)
+    for sentences in sentences_only:
+        words_only = find_words_from_sentence(sentences)
+        for word in words_only:
+            new_list.append(word)
+    return new_list
+
+
+print(find_words_from_sentences_only("Minu nimi on uljana. Python is my fav language!!! I love Python!  ffff44 I love coding. HHhh. ldhrö 3300blabla bla abdscjdll"))
+
+
+def find_years(text: str) -> list:
+    """Given string text, return a list of all 4-digit numbers (years) in that string."""
+    new_list = []
+    pattern = r"\d+"
+    for match in re.finditer(pattern, text):
+        found_words = match.group()
+        new_list.append(found_words)
+    return new_list
+
+
+print(find_years("dd  jvjos 1993. fpxjnsnz2001 oxobdy 2000 ! kksh hslsjijci 4999."))
+
