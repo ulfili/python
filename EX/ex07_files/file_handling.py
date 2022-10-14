c9811137b859003212c900b62ea78613b8fcbc5d
Uljana <ulfili@ttu.ee>
2022-10-14 11:24:21 +0300

    sec function check

--- EX/ex07_files/file_handling.py
+++ EX/ex07_files/file_handling.py
@@ -13,3 +13,21 @@ def read_file_contents(filename: str) -> str:
     with open (filename) as file:
         content = file.read()
     return content
+
+
+def read_file_contents_to_list(filename: str) -> list:
+    """
+    Read file contents into list of lines.
+
+    In this exercise, we can assume the file exists.
+    Each line from the file should be a separate element.
+    The order of the list should be the same as in the file.
+
+    List elements should not contain new line (\n).
+
+    :param filename: File to read.
+    :return: List of lines.
+    """
+    with open (filename) as file:
+        content = file.read()
+    return list(content)
