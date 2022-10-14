b71dd81645be4090f688c3c924bed9b4fb2a884e
Uljana <ulfili@ttu.ee>
2022-10-14 11:22:11 +0300

    first function check

---
+++ EX/ex07_files/file_handling.py
@@ -0,0 +1,15 @@
+"""FILE."""
+
+
+def read_file_contents(filename: str) -> str:
+    """
+    Read file contents into string.
+
+    In this exercise, we can assume the file exists.
+
+    :param filename: File to read.
+    :return: File contents as string.
+    """
+    with open (filename) as file:
+        content = file.read()
+    return content
