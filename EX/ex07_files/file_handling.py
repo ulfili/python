17d4ed05e93433bb8f63e1c0d7f3abb133a132a0
Uljana <ulfili@ttu.ee>
2022-10-16 17:20:07 +0300

    lines to file function

--- EX/ex07_files/file_handling.py
+++ EX/ex07_files/file_handling.py
@@ -11,7 +11,7 @@ def read_file_contents(filename: str) -> str:
     :param filename: File to read.
     :return: File contents as string.
     """
-    with open(filename) as file:
+    with open(filename, "rt") as file:
         content = file.read()
     return content
 
@@ -30,7 +30,7 @@ def read_file_contents_to_list(filename: str) -> list:
     :return: List of lines.
     """
     my_list = []
-    with open(filename) as file:
+    with open(filename, "rt") as file:
         for line in file:
             my_list.append(line.strip())
     return my_list
@@ -39,9 +39,26 @@ def read_file_contents_to_list(filename: str) -> list:
 def read_csv_file(filename: str) -> list:
     """Read CSV file into list of rows."""
     my_list = []
-    with open(filename) as csv_file:
+    with open(filename, "rt") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             my_list.append(row)
     return my_list
 
+
+def write_contents_to_file(filename: str, contents: str) -> None:
+    """Write contents to file."""
+    with open(filename, "w") as file:
+        file.write(contents)
+    pass
+
+
+def write_lines_to_file(filename: str, lines: list) -> None:
+    """Write lines to file."""
+    with open(filename, "a",) as file:
+        for strings in lines:
+            file.write(strings + "\n")
+    pass
+
+
+write_lines_to_file("lines_file", ["list", "of", "stings"])
