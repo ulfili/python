005f0a70a7081e56b9351f2560d4cde8a666dc0c
Uljana <ulfili@ttu.ee>
2022-10-16 17:49:35 +0300

    koma added

--- EX/ex07_files/file_handling.py
+++ EX/ex07_files/file_handling.py
@@ -3,32 +3,14 @@ import csv
 
 
 def read_file_contents(filename: str) -> str:
-    """
-    Read file contents into string.
-
-    In this exercise, we can assume the file exists.
-
-    :param filename: File to read.
-    :return: File contents as string.
-    """
+    """Read file contents into string."""
     with open(filename, "rt") as file:
         content = file.read()
     return content
 
 
 def read_file_contents_to_list(filename: str) -> list:
-    """
-    Read file contents into list of lines.
-
-    In this exercise, we can assume the file exists.
-    Each line from the file should be a separate element.
-    The order of the list should be the same as in the file.
-
-    List elements should not contain new line (\n).
-
-    :param filename: File to read.
-    :return: List of lines.
-    """
+    """Read file contents into list of lines."""
     my_list = []
     with open(filename, "rt") as file:
         for line in file:
@@ -67,7 +49,7 @@ write_lines_to_file("lines_file", ["list", "of", "stings"])
 def write_csv_file(filename: str, data: list) -> None:
     """Write data into CSV file."""
     with open(filename, 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter=";")
+        csv_writer = csv.writer(csv_file, delimiter=",")
         for row in data:
             csv_writer.writerow(row)
     pass
