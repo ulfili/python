244c222c3176ec8f377166c10a67c0e00e8125ed
Uljana <ulfili@ttu.ee>
2022-10-19 11:13:23 +0300

    solutions fruit order made new funtsion

--- EX/ex08_solution_and_tests/solution.py
+++ EX/ex08_solution_and_tests/solution.py
@@ -23,16 +23,16 @@ def fruit_order(small_baskets: int, big_baskets: int, ordered_amount: int) -> in
     all_kg = five_kg_baskets + one_kg_baskets
     if five_kg_baskets == ordered_amount:
         return 0
-    if all_kg == ordered_amount:
+    if ordered_amount == all_kg:
         return small_baskets
-    if all_kg != ordered_amount:
-        if big_baskets == 0 and small_baskets > ordered_amount:
+    if big_baskets == 0:
+        if small_baskets > ordered_amount:
             return ordered_amount
-        else:
-            return -1
+    if all_kg != ordered_amount:
+        return -1
 
 
-print(fruit_order(4, 1, 9))
-print(fruit_order(3, 1, 10))
-print(fruit_order(10, 0, 9))
 print(fruit_order(3, 3, 15))
+print(fruit_order(3, 1, 8))
+print(fruit_order(10, 0, 9))
+print(fruit_order(3, 15, 10))
