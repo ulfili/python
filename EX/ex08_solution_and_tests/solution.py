2dff25c209878e0746ac5cbd6cd51514890d9794
Uljana <ulfili@ttu.ee>
2022-10-19 19:55:23 +0300

    solution fruits function

--- EX/ex08_solution_and_tests/solution.py
+++ EX/ex08_solution_and_tests/solution.py
@@ -13,27 +13,23 @@ def lottery(a: int, b: int, c: int) -> int:
         return 0
 
 
-print(lottery(5, 1, 5))
-
-
 def fruit_order(small_baskets: int, big_baskets: int, ordered_amount: int) -> int:
     """Return number of small fruit baskets if it's possible to finish the order, otherwise return -1."""
-    five_kg_baskets = big_baskets * 5
-    one_kg_baskets = small_baskets * 1
-    all_kg = five_kg_baskets + one_kg_baskets
-    print(all_kg)
-    if five_kg_baskets == ordered_amount:
-        return 0
-    if ordered_amount == all_kg:
+    five_kg = 5
+    five_kg_baskets = ordered_amount // five_kg
+    print("5 kg: ", five_kg_baskets)
+    if five_kg_baskets <= big_baskets:
         return small_baskets
-    if big_baskets == 0:
-        if small_baskets > ordered_amount:
-            return ordered_amount
-    if all_kg != ordered_amount:
+    all_kg = (five_kg_baskets * big_baskets) + (small_baskets * 1)
+    if all_kg == ordered_amount:
+        return small_baskets
+    else:
         return -1
 
 
 print(fruit_order(1, 5, 21))
+print(fruit_order(4, 1, 9))
+print(fruit_order(3, 1, 10))
 
 
 def students_study(time: int, coffee_needed: bool) -> bool:
@@ -48,9 +44,3 @@ def students_study(time: int, coffee_needed: bool) -> bool:
             return False
         else:
             return True
-
-
-print(students_study(17, False))
-print(students_study(18, True))
-print(students_study(19, False))
-print(students_study(1, True))
