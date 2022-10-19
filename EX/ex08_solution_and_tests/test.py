5f29838592c95655870e50bc902f4e8ca3ddbd27
Uljana <ulfili@ttu.ee>
2022-10-19 21:27:20 +0300

    test of solutions coffee funcx10

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -4,12 +4,14 @@ from solution import students_study
 
 def test_student_study__evening_not_coffee_needed():
     """In the evening it doesn't matter whether we have coffee."""
-    assert students_study(20, False) is True
+    assert students_study(20, True) is False
+    assert students_study(20, False)is True
 
 
 def test_student_study__night_study():
     """At night in doesn't matter whether we have coffee."""
-    assert students_study(2, False) is True
+    assert students_study(2, True) is False
+    assert students_study(2, True) is False
 
 
 def test_student_study__day_study():
