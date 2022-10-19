52a237a004a23e8a6110e6493c520c8268544ef3
Uljana <ulfili@ttu.ee>
2022-10-19 20:42:56 +0300

    test of solutions coffee func

---
+++ EX/ex08_solution_and_tests/test.py
@@ -0,0 +1,21 @@
+import pytest
+
+from solution import students_study
+
+
+def test_student_study__evening_not_coffee_needed():
+    """In the evening it doesn't matter whether we have coffee."""
+    assert students_study(19, True) is False
+    assert students_study(19,False) is True
+
+
+def test_student_study__night_study():
+    """At night in doesn't matter whether we have coffee."""
+    assert students_study(1, True) is False
+    assert students_study(1, False) is False
+
+
+def test_student_study__day_study():
+    """Coffee is needed in the morning."""
+    assert students_study(14, True) is True
+    assert students_study(14, False) is False
