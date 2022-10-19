14f5e85e3aeb70bbdc825abdfd0e5ae066a41a32
Uljana <ulfili@ttu.ee>
2022-10-19 10:43:35 +0300

    solutions

---
+++ EX/ex08_solution_and_tests/solution.py
@@ -0,0 +1,31 @@
+"""Solution and test."""
+
+
+def lottery(a: int, b: int, c: int) -> int:
+    """Return Lottery victory result 10, 5, 1, or 0 according to input values."""
+    if a == b == c == 5:
+        return 10
+    if a == b == c != 5:
+        return 5
+    if a != b != c:
+        return 1
+    else:
+        return 0
+
+
+print(lottery(3, 2, 2))
+
+
+def fruit_order(small_baskets: int, big_baskets: int, ordered_amount: int) -> int:
+    """Return number of small fruit baskets if it's possible to finish the order, otherwise return -1."""
+    five_kg_baskets = big_baskets * 5
+    one_kg_baskets = small_baskets * 1
+    all_kg = five_kg_baskets + one_kg_baskets
+    if all_kg == ordered_amount:
+        return small_baskets
+    if all_kg != ordered_amount:
+        return -1
+
+
+print(fruit_order(4, 1, 9))
+print(fruit_order(3, 1, 10))
