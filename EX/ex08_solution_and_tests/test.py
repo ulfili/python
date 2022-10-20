bf774d2845940ec949de785f4844c02e3993c05c
Uljana <ulfili@ttu.ee>
2022-10-20 12:56:00 +0300

    ex08 test

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -1,32 +1,10 @@
 """Test of solutions."""
 import pytest
 from solution import students_study
-from solution import lottery
 
 
 def test_student_study__evening_not_coffee_needed():
     """In the evening it doesn't matter whether we have coffee."""
     assert students_study(20, True) is False
     assert students_study(20, False) is True
-
-
-def test_student_study__night_study():
-    """At night in doesn't matter whether we have coffee."""
-    assert students_study(2, True) is False
-    assert students_study(2, False) is True
-
-
-def test_student_study__day_study():
-    """Coffee is needed in the morning!."""
-    assert students_study(14, True) is True
-    assert students_study(14, False) is False
-
-
-def test_lottery__all_five():
-    """If all numbers are 5."""
-    assert lottery(5, 5, 5) == 10
-
-
-def test_lottery__all_same():
-    """If all numbers are same (expected 5)."""
-    assert lottery(1, 1, 1) == 5
+    assert students_study(20, False) is False
