9b46a8c54a6d32010322474151c7e28e2a3d1853
Uljana <ulfili@ttu.ee>
2022-10-20 14:38:20 +0300

    ex08 test

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -7,6 +7,7 @@ def test_student_study__evening_not_coffee_needed():
     """In the evening it doesn't matter whether we have coffee."""
     assert students_study(20, True) is True
     assert students_study(20, False) is True
+    assert students_study(25, False) is False
 
 
 def test_student_study__night_coffee_needed():
