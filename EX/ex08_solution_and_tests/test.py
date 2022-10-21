f97adc305c0362f450ee895d6152dbce70f8da58
Uljana <ulfili@ttu.ee>
2022-10-21 15:25:20 +0300

    ex08 test

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -88,3 +88,5 @@ def test_fruit_order__not_enough_baskets():
 def test_fruit_order__more_than_required():
     """There are too many baskets."""
     assert fruit_order(5, 0, 4) == 4
+    assert fruit_order(0, 2, 5) == 0
+    assert fruit_order(1, 5, 21) == 1
