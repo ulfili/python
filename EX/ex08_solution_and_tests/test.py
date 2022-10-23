759f8ce306e7d00c2e91bd2f4888646acf03c957
Uljana <ulfili@ttu.ee>
2022-10-23 20:07:29 +0300

    ex08 test

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -25,9 +25,18 @@ def test_student_study__day_coffee_needed():
 
 def test_student_study__edge_case():
     """Something."""
-    assert students_study(17, True) is False
-    assert students_study(18, True) is False
-    assert students_study(23, True) is False
+    assert students_study(1, True) is False
+    assert students_study(1, False) is False
+    assert students_study(4, False) is False
+    assert students_study(4, True) is False
+    assert students_study(5, True) is True
+    assert students_study(5, False) is False
+    assert students_study(17, True) is True
+    assert students_study(17, False) is False
+    assert students_study(18, True) is True
+    assert students_study(18, False) is True
+    assert students_study(24, True) is True
+    assert students_study(24, False) is True
 
 
 def test_lottery__all_same():
