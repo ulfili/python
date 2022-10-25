16dd299021e6204ef5032236031a2b78202501e0
Uljana <ulfili@ttu.ee>
2022-10-25 16:17:01 +0300

    ex09 first functions test

--- EX/ex09_recursion/recursion.py
+++ EX/ex09_recursion/recursion.py
@@ -3,5 +3,64 @@
 
 def loop_reverse(s: str) -> str:
     """Reverse a string."""
-    s = s[::-1]
-    return s
+    reversed_string = ""
+    for i in s:
+        reversed_string = i + reversed_string
+    return reversed_string
+
+
+print(loop_reverse("hey"))
+
+
+def recursive_reverse(s: str) -> str:
+    """Reverse a string using recursion."""
+    if len(s) == 0:
+        return s
+    else:
+        return recursive_reverse(s[1:]) + s[0]
+
+
+print(recursive_reverse("hey"))
+
+
+def loop_sum(n: int) -> int:
+    """Sum of numbers using loop."""
+    num = 0
+    for i in range(1, n+1):
+        num = i + num
+    return num
+
+
+print(loop_sum(5))
+
+
+def recursive_sum(n: int) -> int:
+    """Sum of numbers using recursion."""
+    if n == 0:
+        return n
+    return n + recursive_sum(n-1)
+
+
+print(recursive_sum(5))
+
+
+def countdown(n: int):
+    """Make list of numbers from n to 0."""
+    if n < 0:
+        return []
+    else:
+        return [n] + countdown(n-1)
+
+
+print(countdown(10))
+print(countdown(-1))
+
+
+def add_commas(n: int):
+    """Add comas into a number."""
+    return "{:,}".format(n)
+
+
+print(add_commas(123456))
+print(add_commas(1245))
+print(add_commas(123456789))
