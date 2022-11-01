640218a6f1547dabc86549d5e24b8f0c11e826f1
Uljana <ulfili@ttu.ee>
2022-11-01 12:59:15 +0200

    oop mx ex

---
+++ MX/mx_simple_oop/student.py
@@ -0,0 +1,14 @@
+"""Simple OOP."""
+
+
+class Student:
+    """Student class."""
+    def __init__(self, name, finished):
+        """Student constructor."""
+        self.name = name
+        self.finished = finished
+
+
+student = Student("John", False)
+print(student.name)
+print(student.finished)
