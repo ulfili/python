2f4a5b5955f391b2bb85789c66fe30e26242a410
Uljana <ulfili@ttu.ee>
2022-11-02 14:49:57 +0200

    oop mx

--- MX/mx_simple_oop/student.py
+++ MX/mx_simple_oop/student.py
@@ -4,12 +4,12 @@
 class Student:
     """Student class."""
 
-    def __init__(self, name, finished):
+    def __init__(self, name, finished = False):
         """Student constructor."""
         self.name = name
         self.finished = finished
 
 
-student = Student("John", False)
+student = Student("John")
 print(student.name)
 print(student.finished)
