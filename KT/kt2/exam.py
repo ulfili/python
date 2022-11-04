1f117073c26da6cc77ea8311afd5b1586b8eae5c
Uljana <ulfili@ttu.ee>
2022-11-04 10:14:26 +0200

    first exam exercise

---
+++ KT/kt2/exam.py
@@ -0,0 +1,74 @@
+"""KT2."""
+
+
+def switch_lasts_and_firsts(s: str) -> str:
+    """
+    Move last two characters to the beginning of string and first two characters to the end of string.
+
+    When string length is smaller than 4, return reversed string.
+
+    switch_lasts_and_firsts("ambulance") => "cebulanam"
+    switch_lasts_and_firsts("firetruck") => "ckretrufi"
+    switch_lasts_and_firsts("car") => "rac"
+
+    :param s:
+    :return: modified string
+    """
+    if len(s) <= 4:
+        return s[::-1]
+    else:
+        word = s.replace(s[0:2], s[-2:])
+        return word
+
+
+print(switch_lasts_and_firsts("nana"))
+print(switch_lasts_and_firsts("ambulance"))
+
+
+def take_partial(text: str, leave_count: int, take_count: int) -> str:
+    """
+    Take only part of the string.
+
+    Ignore first leave_count symbols, then use next take_count symbols.
+    Repeat the process until the end of the string.
+
+    The following conditions are met (you don't have to check those):
+    leave_count >= 0
+    take_count >= 0
+    leave_count + take_count > 0
+
+    take_partial("abcdef", 2, 3) => "cde"
+    take_partial("abcdef", 0, 1) => "abcdef"
+    take_partial("abcdef", 1, 0) => ""
+    take_partial("Hello world", 3, 3) => "lo ld"
+    """
+    pass
+
+
+def min_diff(nums: list) -> int:
+    """
+    Find the smallest diff between two integer numbers in the list.
+
+    The list will have at least 2 elements.
+
+    min_diff([1, 2, 3]) => 1
+    min_diff([1, 9, 17]) => 8
+    min_diff([100, 90]) => 10
+    min_diff([1, 100, 1000, 1]) => 0
+
+    :param nums: list of ints, at least 2 elements.
+    :return: min diff between 2 numbers.
+    """
+    pass
+
+
+def get_symbols_by_occurrences(text: str) -> dict:
+    """
+    Return dict where key is the occurrence count and value is a list of corresponding symbols.
+
+    The order of the counts and the symbols is not important.
+
+    get_symbols_by_occurrences("hello") => {1: ['e', 'o', 'h'], 2: ['l']}
+    get_symbols_by_occurrences("abcaba") => {2: ['b'], 1: ['c'], 3: ['a']}
+    """
+    pass
