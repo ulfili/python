f0d48591fe00d75589213f0a68a103942e0d6ca8
Uljana <ulfili@ttu.ee>
2022-11-05 15:21:20 +0200

    first exam exercise

--- KT/kt3/exam.py
+++ KT/kt3/exam.py
@@ -42,63 +42,16 @@ def only_one_pair(numbers: list) -> bool:
     only_one_pair([1, 2, 1, 3, 1, 2]) => False
     """
     no_duplicates = list(set(numbers))
+    # print(no_duplicates)
     for a in no_duplicates:
         numbers.remove(a)
-    print(numbers)
+    # print(numbers)
     if len(numbers) == 1:
         return True
     else:
         return False
 
 
-print(only_one_pair([1, 2, 1, 3, 1, 2]))
+print(only_one_pair([1, 2, 3, 1]))
 print(only_one_pair([1, 2, 3, 3]))
-
-
-def pentabonacci(n: int) -> int:
-    """
-    Find the total number of odd values in the sequence up to the f(n) [included].
-
-    The sequence is defined like this:
-    f(0) = 0
-    f(1) = 1
-    f(2) = 1
-    f(3) = 2
-    f(4) = 4
-    f(n) = f(n - 1) + f(n - 2) + f(n - 3) + f(n - 4) + f(n - 5)
-
-    Keep in mind that 1 is the only value that is duplicated in the sequence
-    and must be counted only once.
-
-    pentabonacci(5) -> 1
-    pentabonacci(10) -> 3
-    pentabonacci(15) -> 5
-
-    :param n: The last term to take into account.
-    :return: Total number of odd values.
-    """
-    pass
-
-
-def swap_dict_keys_and_value_lists(d: dict) -> dict:
-    """
-    Swap keys and values in dict.
-
-    Values are lists.
-    Every element in this list should be a key,
-    and current key will be a value for the new key.
-    Values in the result are lists.
-
-    Every list in input dict has at least 1 element.
-    The order of the values in the result dict is not important.
-
-    swap_dict_keys_and_value_lists({"a": ["b", "c"]}) => {"b": ["a"], "c": ["a"]}
-    swap_dict_keys_and_value_lists({1: [2, 3], 4: [2, 5]}) => {2: [1, 4], 3: [1], 5: [4]}
-    swap_dict_keys_and_value_lists({}) => {}
-    swap_dict_keys_and_value_lists({1: [2]}) => {2: [1]}
-    """
-    swap_dict = {}
-
-
-
-print(swap_dict_keys_and_value_lists({"a": ["b", "c"]}))
+print(only_one_pair([1, 2, 1, 3, 1]))
