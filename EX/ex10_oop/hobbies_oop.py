af48c6af91c63ddf10b551a5edebebf113d641ff
Uljana <ulfili@ttu.ee>
2022-11-08 12:49:47 +0200

    ex10 hobbies

--- EX/ex10_oop/hobbies_oop.py
+++ EX/ex10_oop/hobbies_oop.py
@@ -63,7 +63,7 @@ def sort_by_most_hobbies(people_list: list) -> list:
     :param people_list: list of people to sort.
     :return: sorted list of people.
     """
-    returned_list = sorted(people_list, key=lambda person: len(person.hobbies))
+    returned_list = sorted(people_list, key=lambda person: len(person.hobbies), reverse=True)
     returned_list_2 = sorted(returned_list, key=attrgetter("full_name"))
     return returned_list_2
 
