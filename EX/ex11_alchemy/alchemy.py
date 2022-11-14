0b14b0ade09402cc46d1be592a40179a25530de7
Uljana <ulfili@ttu.ee>
2022-11-14 19:41:54 +0200

    alchemy ex

---
+++ EX/ex11_alchemy/alchemy.py
@@ -0,0 +1,153 @@
+"""Alchemy."""
+
+
+class AlchemicalElement:
+    """
+    AlchemicalElement class.
+
+    Every element must have a name.
+    """
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def __repr__(self):
+        return "AE: " + self.name
+
+
+class AlchemicalStorage:
+    """AlchemicalStorage class."""
+
+    def __init__(self):
+        """
+        Initialize the AlchemicalStorage class.
+
+        You will likely need to add something here, maybe a list?
+        """
+        self.storage = []
+
+
+    def add(self, element: AlchemicalElement):
+        """
+        Add element to storage.
+
+        Check that the element is an instance of AlchemicalElement, if it is not, raise the built-in TypeError exception.
+
+        :param element: Input object to add to storage.
+        """
+        if isinstance(element, AlchemicalElement):
+            self.storage.append(element)
+        else:
+            raise TypeError
+
+
+    def pop(self, element_name: str) -> AlchemicalElement or None:
+        """
+        Remove and return previously added element from storage by its name.
+
+        If there are multiple elements with the same name, remove only the one that was added most recently to the
+        storage. If there are no elements with the given name, do not remove anything and return None.
+
+        :param element_name: Name of the element to remove.
+        :return: The removed AlchemicalElement object or None.
+        """
+        index = -1    # et aru saada millal elementi ei ole
+        i = 0         # elemendi loetelu algab nullist
+        for element in self.storage:
+            print("element is: " + str(element))
+            if element.name == element_name:
+                index = i  # kui element on leitud, siis me salvestame indeksi numbri
+                print("element presents in our storage! " + str(element))
+            i = i + 1    # iga elemendi indeks, salvestab viimase
+        print("index is :" + str(index))
+        if index == -1:
+            return None
+        return self.storage.pop(index)
+
+
+    def extract(self) -> list[AlchemicalElement]:
+        """
+        Return a list of all of the elements from storage and empty the storage itself.
+
+        Order of the list must be the same as the order in which the elements were added.
+
+        Example:
+            storage = AlchemicalStorage()
+            storage.add(AlchemicalElement('Water'))
+            storage.add(AlchemicalElement('Fire'))
+            storage.extract() # -> [<AE: Water>, <AE: Fire>]
+            storage.extract() # -> []
+
+        In this example, the second time we use .extract() the output list is empty because we already extracted
+         everything.
+
+        :return: A list of all of the elements that were previously in the storage.
+        """
+        return []
+
+
+    def get_content(self) -> str:
+        """
+        Return a string that gives an overview of the contents of the storage.
+
+        Example:
+            storage = AlchemicalStorage()
+            storage.add(AlchemicalElement('Water'))
+            storage.add(AlchemicalElement('Fire'))
+            print(storage.get_content())
+
+        Output in console:
+            Content:
+             * Fire x 1
+             * Water x 1
+
+        The elements must be sorted alphabetically by name.
+
+        :return: Content as a string.
+        """
+        return ''
+
+
+    def __repr__(self):
+        return "alchemical storage content is: " + str(self.storage)
+
+
+
+if __name__ == '__main__':
+    element_one = AlchemicalElement('Fire')
+    element_two = AlchemicalElement('Water')
+    element_six = AlchemicalElement('Earth')
+    element_three = AlchemicalElement('Water')
+    element_four = AlchemicalElement('Air')
+    storage = AlchemicalStorage()
+
+    print(element_one)  # <AE: Fire>
+    print(element_two)  # <AE: Water>
+    storage.add(element_one)
+    storage.add(element_two)
+    storage.add(element_six)
+    storage.add(element_three)
+    storage.add(element_four)
+    print(storage)
+    print(storage.pop("WATRE"))
+    print(storage)
+
+    """
+    print(storage.get_content())
+    
+    # Content:
+    #  * Fire x 1
+    #  * Water x 1
+    
+    print(storage.extract())  # [<AE: Fire>, <AE: Water>]
+    print(storage.get_content())
+    # Content:
+    #  Empty
+    
+    storage.add(element_one)
+    storage.add(element_two)
+    storage.add(element_three)
+
+    print(storage.pop('Water') == element_three)  # True
+    print(storage.pop('Water') == element_two)  # True
+    """
