a05c0f2e1377d9b8e3496190688690a8e16bca37
Uljana <ulfili@ttu.ee>
2022-11-15 19:19:39 +0200

    exam example

--- EXAM/exam0/exam.py
+++ EXAM/exam0/exam.py
@@ -3,6 +3,7 @@ import re
 
 
 def find_capital_letters(text: str) -> str:
+    """Function that finds capital letters."""
     pattern = r"[A-Z]"
     capital_letters = ""
     for match in re.finditer(pattern, text):
@@ -11,3 +12,16 @@ def find_capital_letters(text: str) -> str:
 
 
 print(find_capital_letters("Tomato AppLE cat Dog HoMe"))
+
+
+def close_far(a: int, b: int, c: int) -> bool:
+    """Docstring."""
+    if a - b <= 1:
+        return True
+    if a - c <= 1:
+        return True
+    else:
+        return False
+
+
+print(close_far(3, 1, 1))
