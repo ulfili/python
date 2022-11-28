28dfcfe74094981b6e7e6e3dac430069944511ca
Uljana <ulfili@ttu.ee>
2022-11-28 17:31:13 +0200

    wat

--- WAT/wat/wat.py
+++ WAT/wat/wat.py
@@ -1,19 +1,19 @@
 """WAT."""
 
 
-def function_a():
+def function_a(a: str):
     """Function a."""
 
 
-def function_b():
+def function_b(b: str):
     """Function b."""
 
 
-def function_c():
+def function_c(c: str):
     """Function c."""
 
 
-def function_d():
+def function_d(d: str):
     """Function d."""
 
 
@@ -21,13 +21,13 @@ def function_e():
     """Function e."""
 
 
-def function_f():
+def function_f(f: str):
     """Function f."""
 
 
-def function_g():
+def function_g(g: str):
     """Function g."""
 
 
-def function_h():
+def function_h(h: str):
     """Function h."""
