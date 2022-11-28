578da9c1bae463ab779672342f63023b5c459c1f
Uljana <ulfili@ttu.ee>
2022-11-28 17:33:57 +0200

    wat

--- WAT/wat/wat.py
+++ WAT/wat/wat.py
@@ -3,31 +3,39 @@
 
 def function_a(a: str):
     """Function."""
+    return True
 
 
 def function_b(b: str):
     """Function."""
+    return True
 
 
 def function_c(c: str):
     """Function."""
+    return True
 
 
 def function_d(d: str):
     """Function."""
+    return True
 
 
 def function_e(e: str):
     """Function."""
+    return True
 
 
 def function_f(f: str):
     """Function."""
+    return True
 
 
 def function_g(g: str):
     """Function."""
+    return True
 
 
 def function_h(h: str):
     """Function."""
+    return True
