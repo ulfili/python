acef0b8746c6a343f902a88a8e87f44c68f551e8
Uljana <ulfili@ttu.ee>
2022-12-01 19:13:54 +0200

    robot.py

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -10,8 +10,7 @@ def test_run(robot: FollowerBot):
 
     :param FollowerBot robot: instance of the robot that you need to make move
     """
-    #robot = FollowerBot()
-
+    # robot = FollowerBot()
     robot.set_wheels_speed(30)
     robot.sleep(2)
     robot.set_wheels_speed(0)
