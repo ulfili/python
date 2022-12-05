0e7d5ef668b7a3bd730859d816e6fbaa74a17c5f
Uljana <ulfili@ttu.ee>
2022-12-05 18:04:29 +0200

    robot line following

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -18,3 +18,35 @@ def test_run(robot: FollowerBot):
 
 
 test_run(robot=FollowerBot())
+
+
+def drive_to_line(robot: FollowerBot):
+    """
+    Drive the robot until it meets a perpendicular black line, then drive forward 25cm.
+
+    There are 100 pixels in a meter.
+
+    :param FollowerBot robot: instance of the robot that you need to make move
+    """
+    pass
+
+
+def follow_the_line(robot: FollowerBot):
+    """
+    Create a FollowerBot that will follow a black line until the end of that line.
+
+    The robot's starting position will be just short of the start point of the line.
+
+    :param FollowerBot robot: instance of the robot that you need to make move
+    """
+    robot.set_wheels_speed(-50)
+    robot.sleep(1)
+    print(robot.get_position())
+    print(robot.get_line_sensors())
+    robot.set_wheels_speed(0)
+    robot.done()
+
+
+if __name__ == '__main__':
+    robot = FollowerBot(track_image="line.png", start_x=172, start_y=430)
+    follow_the_line(robot)
