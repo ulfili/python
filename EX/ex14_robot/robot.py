8a6643ed7ef8b2ce296b4a87e935210abf551f82
Uljana <ulfili@ttu.ee>
2022-12-06 22:27:03 +0200

    robot follow the line

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -68,14 +68,14 @@ def follow_the_line(robot: FollowerBot):
             # print(robot.get_line_sensors())
             # print(robot.get_position())
         if robot.get_left_line_sensor() == 0 and robot.get_right_line_sensor() != 0:
-            robot.set_right_wheel_speed(30)
-            robot.set_left_wheel_speed(-15)
+            robot.set_right_wheel_speed(50)
+            robot.set_left_wheel_speed(-25)
             robot.sleep(0.01)
             print(robot.get_line_sensors())
 
         elif robot.get_left_line_sensor() != 0 and robot.get_right_line_sensor() == 0:
-            robot.set_right_wheel_speed(-15)
-            robot.set_left_wheel_speed(30)
+            robot.set_right_wheel_speed(-25)
+            robot.set_left_wheel_speed(50)
             robot.sleep(0.01)
             print(robot.get_line_sensors())
 
