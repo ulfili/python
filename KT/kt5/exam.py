b171499f44d0ccac6fa08275ca8b0c83fd085029
Uljana <ulfili@ttu.ee>
2023-01-08 14:32:51 +0200

    kt5

--- KT/kt5/exam.py
+++ KT/kt5/exam.py
@@ -7,10 +7,10 @@ def get_date_string(date: list) -> str:
         return "The date is unknown!"
 
     else:
-        return "The date is " + str(date[0]) + "/" + str(date[1]) + "/" + str(date[2])
+        return "The date is -> " + str(date[0]) + "/" + str(date[1]) + "/" + str(date[2])
 
 
-print(get_date_string([3, 3, 2000])) # -> "The date is -> 3/3/2000"
-print(get_date_string([20, 12, 5677])) # -> "The date is -> 20/12/5677"
-print(get_date_string([2, 2, 3, 200])) # -> "The date is unknown!"
-print(get_date_string([])) # -> "The date is unknown!"
+print(get_date_string([3, 3, 2000]))  # -> "The date is -> 3/3/2000"
+print(get_date_string([20, 12, 5677]))  # -> "The date is -> 20/12/5677"
+print(get_date_string([2, 2, 3, 200]))  # -> "The date is unknown!"
+print(get_date_string([]))  # -> "The date is unknown!"
