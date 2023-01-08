a030b372e5de559e7d5db04a6da4979aa7456ada
Uljana <ulfili@ttu.ee>
2023-01-08 15:04:08 +0200

    kt5

--- KT/kt5/exam.py
+++ KT/kt5/exam.py
@@ -14,3 +14,53 @@ print(get_date_string([3, 3, 2000]))  # -> "The date is -> 3/3/2000"
 print(get_date_string([20, 12, 5677]))  # -> "The date is -> 20/12/5677"
 print(get_date_string([2, 2, 3, 200]))  # -> "The date is unknown!"
 print(get_date_string([]))  # -> "The date is unknown!"
+
+
+def odd_sums_of_consecutive_elements(nums: list) -> list:
+    """
+    Return list of odd sums of consecutive elements.
+
+    Consider all consecutive elements in the input list. Return a list of all the odd sums of consecutive elements.
+
+    odd_sums_of_consecutive_elements([1, 2, 3, 5]) => [3, 5]
+    odd_sums_of_consecutive_elements([8, 10]) => []
+    odd_sums_of_consecutive_elements([9]) => []
+    odd_sums_of_consecutive_elements([11, 8]) => [19]
+
+    :param nums:
+    :return:
+    """
+    sums_list = []
+    odd_nums = []
+    for i in range(len(nums) - 1):
+        new_nums = (nums[i] + nums[i + 1])
+        sums_list.append(new_nums)
+    #print(sums_list)
+    for elem in sums_list:
+        #print(elem)
+        if elem % 2 > 0:
+            odd_nums.append(elem)
+            return odd_nums
+        if elem % 2 == 0:
+            return []
+    if len(nums) <= 1:
+        return []
+
+
+print(odd_sums_of_consecutive_elements([1, 2, 3, 5]))  # => [3, 5]
+print(odd_sums_of_consecutive_elements([8, 10]))  # => []
+print(odd_sums_of_consecutive_elements([9]))  # => []
+print(odd_sums_of_consecutive_elements([11, 8]))  # => [19]
+
+
+def g_happy(s: str) -> bool:
+    """
+    We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
+
+    Return True if all the g's in the given string are happy.
+
+    g_happy("xxggxx") => True
+    g_happy("xxgxx") => False
+    g_happy("xxggyygxx") => False
+    """
+    pass
