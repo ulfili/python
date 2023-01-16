c32bfef5d35afb96d8d5ae55d1bdffd93435ff37
Uljana <ulfili@ttu.ee>
2023-01-16 20:35:19 +0200

    exam3

--- EXAM/exam4/exam.py
+++ EXAM/exam4/exam.py
@@ -150,8 +150,12 @@ def count_the_dumplings(day: int) -> int:
     count_the_dumplings(3) => 4
     count_the_dumplings(30) ==> 536870912
     """
-    pass
+    if day == 0:
+        return 0
+    return 2 ** (day - 1)
 
+
+print(count_the_dumplings(30))    # ==> 536870912)
 
 def prime_factorization(number: int) -> int:
     """
